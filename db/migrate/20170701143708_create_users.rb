58babe3de3dd9d4273564080f8a7b600b5ffde4b
Random4405 <random4405@gmail.com>
2017-07-01 18:02:40 +0300

    Run migrations

--- db/migrate/20170701143708_create_users.rb
+++ db/migrate/20170701143708_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def up
     create_table :users do |t|
       t.column "first_name", :string, :limit => 25
-      t.string "last_name", :klimit => 50
+      t.string "last_name", :limit => 50
       t.string "email", :default => '', :null => false
       t.string "password", :limit => 40
 
